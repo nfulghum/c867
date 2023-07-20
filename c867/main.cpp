df3d3749ba844bdd720862da255498ff377bf82f
Nick Fulghum <nickfulghum92@gmail.com>
2023-07-20 12:51:40 -0500

    Initial Commit

---
+++ c867/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c867
+//
+//  Created by Nicholas Fulghum on 7/20/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
