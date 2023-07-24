6252c470d3d3639285299fa973af5c70c5b004b0
Nick Fulghum <nickfulghum92@gmail.com>
2023-07-24 13:41:49 -0500

    fixed errors

--- c867/student.hpp
+++ c867/student.hpp
@@ -60,3 +60,23 @@ public:
 };
 
 #endif /* student_hpp */
+
+/*
+ D.  For the Student class, do the following:
+
+ 1.  Create the class Student  in the files student.h and student.cpp, which includes each of the following variables:
+
+ •  student ID
+
+ •  first name
+
+ •   last name
+
+ •  email address
+
+ •  age
+
+ •  array of number of days to complete each course
+
+ •  degree program
+ */
